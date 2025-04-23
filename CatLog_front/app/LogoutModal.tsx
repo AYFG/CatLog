92c58434f993d9ac00300495439d8b3bf875e343
AYFG <sek3137@naver.com>
2025-04-23 16:08:54 +0900

    fix logoutmodal design

--- CatLog_front/app/LogoutModal.tsx
+++ CatLog_front/app/LogoutModal.tsx
@@ -17,21 +17,18 @@ export default function LogoutModal() {
     <Animated.View className="items-center justify-center flex-1 bg-[#00000040]" entering={FadeIn}>
       <Pressable onPress={() => router.back()} style={StyleSheet.absoluteFill} />
 
-      <Animated.View
-        entering={SlideInDown}
-        className="flex w-3/5 shadow-lg rounded-xl bg-snow h-1/6"
-      >
+      <Animated.View entering={SlideInDown} className="flex w-3/5 shadow-lg rounded-xl bg-snow ">
         <Text className="mt-5 mb-3 ml-5 font-bold">로그아웃</Text>
         <Text className="ml-5">정말 로그아웃할까요?</Text>
-        <View className="flex flex-row items-center justify-center flex-1 gap-2 p-4">
-          <View className="items-center flex-1 p-4 font-bold rounded-lg  bg-[#e9ecef]">
+        <View className="flex flex-row items-center justify-center my-4">
+          <View className=" font-bold flex-1 ml-4 mr-2 py-4 rounded-lg bg-[#e9ecef]">
             <Pressable onPress={() => router.back()}>
-              <Text className="font-bold">취소</Text>
+              <Text className="font-bold text-center">취소</Text>
             </Pressable>
           </View>
-          <View className="items-center flex-1 p-4 rounded-lg bg-wePeep">
+          <View className="items-center flex-1 py-4 mr-4 rounded-lg bg-wePeep">
             <Pressable onPress={handleLogout}>
-              <Text className="font-bold text-snow">확인</Text>
+              <Text className="font-bold text-center text-snow">확인</Text>
             </Pressable>
           </View>
         </View>
