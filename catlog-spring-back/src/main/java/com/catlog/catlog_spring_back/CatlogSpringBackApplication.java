7668fdde17a938020c76f6543195752a6eff392c
AYFG <sek82468246@gmail.com>
2026-05-14 23:06:18 +0900

    spring backend migration 초기세팅

---
+++ catlog-spring-back/src/main/java/com/catlog/catlog_spring_back/CatlogSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.catlog.catlog_spring_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlogSpringBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatlogSpringBackApplication.class, args);
+	}
+
+}
